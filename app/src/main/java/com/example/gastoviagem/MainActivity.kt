58678e6032c123eab92fd43fd40f498555b61ec9
Wiilames <willmiranda.94@gmail.com>
2022-08-21 20:24:16 -0400

    fazendo validacao dos dados recebidos no input

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.widget.Toast
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener{
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,19 +19,32 @@ class MainActivity : AppCompatActivity(), View.OnClickListener{
     }
 
     override fun onClick(view: View) {
-        if(view.id == R.id.button_calculate){
+        if (view.id == R.id.button_calculate) {
             calculate()
         }
+
     }
-    private fun calculate(){
-        val distance = binding.editDistance.text.toString().toFloat()
-        val price = binding.editPrice.text.toString().toFloat()
-        val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-        val totalValue = (distance * price) / autonomy
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+
+    }
+
+    private fun calculate() {
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+            val totalValue = (distance * price) / autonomy
 
 
-        binding.textTotalValue.text ="R$ ${"%.2f".format(totalValue)}"
-        //Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
+            binding.textTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_SHORT).show()
+        }
     }
 }
